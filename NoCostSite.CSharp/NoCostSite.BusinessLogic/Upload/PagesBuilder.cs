e0b6c01765ff34bfc62659f364ae7fc0b4a272de
Roman Puzanov <puzanov.rs@skbkontur.ru>
2022-05-14 20:48:05 +0500

    Valid page build

--- NoCostSite.CSharp/NoCostSite.BusinessLogic/Upload/PagesBuilder.cs
+++ NoCostSite.CSharp/NoCostSite.BusinessLogic/Upload/PagesBuilder.cs
@@ -3,26 +3,26 @@ using System.Collections.Generic;
 using NoCostSite.BusinessLogic.Pages;
 using NoCostSite.BusinessLogic.Templates;
 
-namespace NoCostSite.BusinessLogic.FilesUpload
+namespace NoCostSite.BusinessLogic.Upload
 {
     public static class PagesBuilder
     {
         public static string Build(Page page, Template template)
         {
             return template.Content
-                .Replace(nameof(Page.Title), page.Title)
-                .Replace(nameof(Page.Description), page.Description)
-                .Replace(nameof(Page.Keywords), page.Keywords)
-                .Replace(nameof(Page.Content), page.Content);
+                .ReplaceTag(nameof(Page.Title), page.Title)
+                .ReplaceTag(nameof(Page.Description), page.Description)
+                .ReplaceTag(nameof(Page.Keywords), page.Keywords)
+                .ReplaceTag(nameof(Page.Content), page.Content);
         }
 
         public static string AsTag(this string tag) => $"<!-- {tag} -->";
 
-        private static string Replace(this string str, string tag, string content)
+        private static string ReplaceTag(this string str, string tag, string content)
         {
             return str.Contains(tag.AsTag())
-                ? content.Replace(tag.AsTag(), TagBuilders[tag](content))
-                : content;
+                ? str.Replace(tag.AsTag(), TagBuilders[tag](content))
+                : str;
         }
 
         private static readonly Dictionary<string, Func<string, string>> TagBuilders =
