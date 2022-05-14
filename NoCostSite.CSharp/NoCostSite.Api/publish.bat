cbcc8afc561d058165e6299408233e8914e908c9
Roman Puzanov <puzanov.rs@skbkontur.ru>
2022-05-14 17:48:44 +0500

    Publish

--- NoCostSite.CSharp/NoCostSite.Api/publish.bat
+++ NoCostSite.CSharp/NoCostSite.Api/publish.bat
@@ -1,6 +1,14 @@
 dotnet publish -c Release
-set /p AccessKeyId=<../../../../tokens/AccessKeyId
-set /p SecretAccessKey=<../../../../tokens/SecretAccessKey
-set /p ServiceUrl=<../../../../tokens/ServiceUrl
-set /p Region=<../../../../tokens/Region
-yc serverless function version create --function-name=no-cost-site --runtime dotnetcore31 --entrypoint NoCostSite.Api.EntryPoint --memory 128m --execution-timeout 3s --service-account-id aje4034bmbfmdstij739 --source-path ".\bin\Release\netcoreapp3.1\publish" --environment AccessKeyId=%AccessKeyId%,SecretAccessKey=%SecretAccessKey%,ServiceUrl=%ServiceUrl%,Region=%Region%
+
+set /p PublicBucketName=<../../../../settings/PublicBucketName
+set /p PrivateBucketName=<../../../../settings/PrivateBucketName
+set /p ObjectStorageAccessKeyId=<../../../../settings/ObjectStorageAccessKeyId
+set /p ObjectStorageSecretAccessKey=<../../../../settings/ObjectStorageSecretAccessKey
+set /p ObjectStorageServiceUrl=<../../../../settings/ObjectStorageServiceUrl
+set /p ObjectStorageRegion=<../../../../settings/ObjectStorageRegion
+set /p TokenIssuer=<../../../../settings/TokenIssuer
+set /p TokenAudience=<../../../../settings/TokenAudience
+set /p TokenSecureKey=<../../../../settings/TokenSecureKey
+set /p TokenExpirationDays=<../../../../settings/TokenExpirationDays
+set /p DataBaseSecureKey=<../../../../settings/DataBaseSecureKey
+yc serverless function version create --function-name=no-cost-site --runtime dotnetcore31 --entrypoint NoCostSite.Api.EntryPoint --memory 128m --execution-timeout 3s --service-account-id aje4034bmbfmdstij739 --source-path ".\bin\Release\netcoreapp3.1\publish" --environment PublicBucketName=%PublicBucketName%,PrivateBucketName=%PrivateBucketName%,ObjectStorageAccessKeyId=%ObjectStorageAccessKeyId%,ObjectStorageSecretAccessKey=%ObjectStorageSecretAccessKey%,ObjectStorageServiceUrl=%ObjectStorageServiceUrl%,ObjectStorageRegion=%ObjectStorageRegion%,TokenIssuer=%TokenIssuer%,TokenAudience=%TokenAudience%,TokenSecureKey=%TokenSecureKey%,TokenExpirationDays=%TokenExpirationDays%,DataBaseSecureKey=%DataBaseSecureKey%
