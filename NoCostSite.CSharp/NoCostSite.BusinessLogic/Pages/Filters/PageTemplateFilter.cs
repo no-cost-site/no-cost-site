824a5399696921f6b5a034a9ae70f07c1b7d897d
Roman Puzanov <puzanov.rs@skbkontur.ru>
2022-05-18 17:58:30 +0500

    Validate delete template

--- NoCostSite.CSharp/NoCostSite.BusinessLogic/Pages/Filters/PageTemplateFilter.cs
+++ NoCostSite.CSharp/NoCostSite.BusinessLogic/Pages/Filters/PageTemplateFilter.cs
@@ -1,9 +1,21 @@
-﻿using NoCostSite.BusinessLogic.Templates;
+﻿using System.Linq;
+using System.Threading.Tasks;
+using NoCostSite.BusinessLogic.Templates;
 using NoCostSite.BusinessLogic.UpdateFilters;
+using NoCostSite.Utils;
 
 namespace NoCostSite.BusinessLogic.Pages.Filters
 {
     public class PageTemplateFilter : UpdateFilterBase<Template>
     {
+        private readonly PagesService _pagesService = new PagesService();
+
+        public override Task BeforeDelete(Template item) => CheckNotExistPageWithTemplate(item);
+
+        private async Task CheckNotExistPageWithTemplate(Template template)
+        {
+            var pages = await _pagesService.ReadAll();
+            Assert.Validate(() => pages.All(x => x.TemplateId != template.Id), "There is a page with a template");
+        }
     }
 }
